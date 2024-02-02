4475538a81df53519e55cced26fa1db1691acfcc
hugolinbergier <hbergier@regis.edu>
2024-02-02 16:30:42 -0700

    add a main for KB translate.

--- RiCO-AI/src/main/java/kbingest/translator/Main.java
+++ RiCO-AI/src/main/java/kbingest/translator/Main.java
@@ -4,12 +4,12 @@ import kbingest.parser.ParseError;
 
 import java.io.IOException;
 
-public class MainTranslator {
+public class Main {
     public static final String SOURCE1 = "//Users//hugolinbergier//dev//Clojure//Query2//kb//faucModule.pl";
     public static final String SOURCE2 = "//Users//hugolinbergier//dev//Clojure//Query2//kb//jobModule.pl";
     public static final String SOURCE3 = "//Users//hugolinbergier//dev//Clojure//Query2//kb//programModule.pl";
 
-    public static void run() throws IOException, ParseError {
+    public static void main(String[] args)  throws IOException, ParseError {
         String[] filePaths = {SOURCE1, SOURCE2, SOURCE3};
         KB myKB = new KB(filePaths);
 //		myKB.functionalWriting();
